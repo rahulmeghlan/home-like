8fcabc9c6e69d1b6ed1ef86b12c7c29825370534
rmeghlan <rmeghlan@wdtablesystems.com>
2018-06-24 14:26:00 +0530

    Integration RepoDetailView with props value

--- src/views/RepoDetailView.js
+++ src/views/RepoDetailView.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import {ApolloConsumer} from 'react-apollo';
 
 export default class RepoDetailView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {detail: null};
+    }
+
+    setRepoDetailDep(client) {
+        this.query = gql`{
+          viewer {
+            repository(name: "${this.props.match.params.reponame}") {
+              object(expression: "master:") {
+              ... on Tree{
+                entries{
+                  name
+                  type
+                  mode
+                }
+              }
+            }
+            }
+          }
+        }`;
+        this.client = client;
+    }
+
+    async getRepoDetail(client, query) {
+        return await client.query({
+            query: query
+        });
+    }
+
+    componentDidMount() {
+        this.getRepoDetail(this.client, this.query).then((res) => {
+            this.setState({
+                detail: res.data.viewer.repository.object.entries
+            });
+        })
+    }
+
     render() {
-        return <div>RepoDetailView</div>
+        return <ApolloConsumer>
+            {client => (
+                <div>
+                    {this.setRepoDetailDep(client)}
+                    {this.state.detail && this.state.detail.map((item, index) => (
+                        <div key={index}>{item.name} ==== {item.type}</div>
+                    ))}
+                </div>
+            )}
+        </ApolloConsumer>
     }
 }
