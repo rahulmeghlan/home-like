893643d7e945c3342d11a74771a9775d0ff59309
rmeghlan <rmeghlan@wdtablesystems.com>
2018-06-24 12:45:59 +0530

    Added route to repo-detail-view

--- src/views/PinnedRepoView.js
+++ src/views/PinnedRepoView.js
@@ -3,9 +3,10 @@ import React from 'react';
 export default class PinnedRepoView extends React.Component {
     render() {
         let {info} = this.props;
+        let url = '/' + info.name;
         return <div className='pinned-repo-item'>
             <div className='pinned-repo-item-content'>
-                <a className='title'>{info.name}</a>
+                <a className='title' href={url}>{info.name}</a>
                 <p className='description'>{info.description}</p>
             </div>
         </div>
