a571b068038f8dca2f32a992f83504f6c85df01d
rmeghlan <rmeghlan@wdtablesystems.com>
2018-06-29 12:50:19 +0530

    Work started for implementing redux in the application

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {ApolloProvider} from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import HomeView from './views/HomeView';
 import RepoDetailView from './views/RepoDetailView';
 import client from './ApolloClient';
@@ -11,14 +12,16 @@ class App extends Component {
     render() {
         return (
             <ApolloProvider client={client}>
-                <Router>
-                    <div>
-                        <Route exact path="/" component={HomeView}/>
-                        <Switch>
-                            <Route exact path="/:reponame" component={RepoDetailView}/>
-                        </Switch>
-                    </div>
-                </Router>
+                <Provider store={store}>
+                    <Router>
+                        <div>
+                            <Route exact path="/" component={HomeView}/>
+                            <Switch>
+                                <Route exact path="/:reponame" component={RepoDetailView}/>
+                            </Switch>
+                        </div>
+                    </Router>
+                </Provider>
             </ApolloProvider>
         );
     }
